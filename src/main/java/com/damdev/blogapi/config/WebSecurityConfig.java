19904073c24bb39315c9dfb907c1d1e3bd397aa2
Jeong Eunwoo <zenic88@naver.com>
2018-10-06 10:12:56 +0900

    #9 시큐리티 설정 추가
    configure 구현

--- src/main/java/com/damdev/blogapi/config/WebSecurityConfig.java
+++ src/main/java/com/damdev/blogapi/config/WebSecurityConfig.java
@@ -2,6 +2,7 @@ package com.damdev.blogapi.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -12,6 +13,16 @@ import org.springframework.security.crypto.password.PasswordEncoder;
  */
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http.authorizeRequests()
+      .antMatchers("/").permitAll()  // url 에 권한 주기
+      .anyRequest().authenticated()
+      .and()
+      .formLogin().and()
+      .httpBasic();
+  }
 
   @Bean
   public PasswordEncoder passwordEncoder() {
